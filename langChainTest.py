a0fda0a2e56c0fe733c71ee188a5e85bb87a9d20
XiaosongGao44314108 <xiaosong.gao@students.mq.edu.au>
2024-03-26 21:32:21 +1100

    remove env
    false config

--- langChainTest.py
+++ langChainTest.py
@@ -1,11 +1,6 @@
-from transformers import AutoModel, AutoTokenizer
-import gradio as gr
-import mdtex2html
-from utils import load_model_on_gpus
+from transformers import AutoTokenizer, AutoModel
+
 
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True)
 model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).half().cuda()
-# 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
-# from utils import load_model_on_gpus
-# model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
 model = model.eval()
--- testChatGLM2.py
+++ testChatGLM2.py
@@ -1,16 +1,7 @@
 from transformers import AutoTokenizer, AutoModel
 
-def main():
-    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True)
-    model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).half().cuda()
-    model = model.eval()
-    response, history = model.chat(tokenizer, "hello", history=[])
-    print(response)
-
-    response, history = model.chat(tokenizer, "can u print a hello world message by python？", history=history)
-    print(response)
-    
-    
-    
-if __name__ == '__main__':
-    main()
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).half().cuda()
+model = model.eval()
+response, history = model.chat(tokenizer, "你好", history=[])
+print(response)
