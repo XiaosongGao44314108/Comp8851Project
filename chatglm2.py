eb6da058e36cbb3403299a553cb9fa93ee8e171f
XiaosongGao44314108 <xiaosong.gao@students.mq.edu.au>
2024-03-26 23:56:46 +1100

    langChian+Chatglm
    connect langChian and Chatglm

---
+++ chatglm2.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+from langchain.llms.base import LLM
+from langchain.llms.utils import enforce_stop_tokens
+from typing import List, Optional
+
+class ChatGLM2(LLM):
+    max_token: int = 2048
+    temperature: float = 0.1
+    top_p = 0.7
+    history = []
+    
+    def __init__(self, temperature=temperature):
+        super().__init__()
+        self.temperature = temperature
+        
+    @property
+    def _llm_type(self) -> str:
+        return "ChatGLM2"
+    
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        headers = {'Content-Type': 'application/json'}
+        data = json.dumps({
+            'prompt':prompt,
+            'temperature':self.temperature, 
+            'history':self.history,
+            'max_length':self.max_token})
+        print("ChatGLM prompt:", prompt)
+        # 调用api 
+        response = requests.post("http://0.0.0.0:8000", headers=headers, data=data, timeout=30)
+        if response.status_code!=200:
+            return "查询结果错误"
+        resp = response.json()
+        if stop is not None:
+            response = enforce_stop_tokens(response, stop)
+        self.history = self.history + [[None, resp['response']]]
+        return resp['response']
--- langChainTest.py
+++ langChainTest.py
@@ -1,6 +1,18 @@
-from transformers import AutoTokenizer, AutoModel
+from langchain import PromptTemplate
+from chatglm2 import ChatGLM2
 
+llm = ChatGLM2(temperature=0.1)
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).half().cuda()
-model = model.eval()
+template = """
+    内容生成任务：请根据下面的提示，生成一段文本。
+    
+    {text}
+"""
+prompt = PromptTemplate(
+   input_variables=["text"],
+   template=template,
+)
+text = '今天是个好天气，'
+prompt_format = prompt.format(text=text)
+output = llm(prompt_format)
+print(output)
